b40dae311c53a5f31ffca477ebdb62c256123785
Harikasan <attipatlaharika@gmail.com>
2026-06-24 12:43:01 -0500

    Update PROTO_DIR path in grpc_clients.js

--- gateway-service/src/grpc_clients.js
+++ gateway-service/src/grpc_clients.js
@@ -2,7 +2,7 @@ const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
 const path = require('path');
 
-const PROTO_DIR = path.join(__dirname, '..', '..', 'proto');
+const PROTO_DIR = path.join(__dirname, '..', 'proto');
 
 const loaderOptions = {
   keepCase: true,
